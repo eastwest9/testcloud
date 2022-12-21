8257a4b5baefc8edbe0c27067c81407e177701cf
eastwest9 <dong092276@gmail.com>
2022-12-21 15:49:05 +0900

    22.12.21

--- application.py
+++ application.py
@@ -6,3 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return render_template('webtest.html')
+
+@app.route('/cat.html')
+def test():
+    return render_template('cat.html')
+
+@app.route('/test.html')
+def test():
+    return render_template('test.html')
